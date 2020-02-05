9aaed2053f6d5b11cd9cc593717468b5893a75c5
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-02-04 22:17:06 -0800

    Implemented generic code for passport authenticate; fixed passport error messages to remove html tags

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -30,7 +30,7 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/game", function (req, res) {
+  app.get("/game", isAuthenticated, function (req, res) {
     db.GameInfo.findAll({}).then(function (data) {
       res.render("game", { data: data });
     });
