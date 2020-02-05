5ddae7508814b46da1fe6957b0b5e08af3f364b6
origami unicorn <rvwright@gmail.com>
2020-02-04 20:05:49 -0800

    Added modal functionality on select of Login from nav menu.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -19,17 +19,6 @@ module.exports = function (app) {
     });
   });
 
-
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-
   app.get("/story/:story_id", function (req, res) {
     db.Story.findOne({
       where: {
